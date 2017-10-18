8952f0e08d1378a6e05aac82d4869e67583d58f3
Michael Okarimia <git@michaelokarimia.com>
2017-10-18 10:18:26 +0100

    Greedy strategy depth first search of BinaryTree

--- Algorithms/ExecutivePay.cs
+++ Algorithms/ExecutivePay.cs
@@ -5,11 +5,32 @@ namespace Algorithms
 {
     internal class ExecutivePay
     {
+        static int leftSum = 0;
+        static int rightSum = 0;
+
         internal static void Run()
         {
             BinaryTree root = buildPayTree();
 
-            depthFirstSearch(root);
+            leftSum = 0;
+            rightSum = 0;
+            calcRightVsLeftSalaries(root);
+            Console.WriteLine("LeftSum: {0}, RightSum: {1}", leftSum, rightSum);
+        }
+
+        private static void calcRightVsLeftSalaries(BinaryTree node)
+        {
+            if(node.LeftTree != null)
+            {
+                leftSum += node.LeftTree.Value;
+                calcRightVsLeftSalaries(node.LeftTree);
+            }
+
+            if (node.RightTree != null)
+            {
+                rightSum += node.RightTree.Value;
+                calcRightVsLeftSalaries(node.RightTree);
+            }
         }
 
         private static void depthFirstSearch(BinaryTree node)
@@ -31,8 +52,12 @@ namespace Algorithms
             var coo = ceo.AddRight(500);
 
             var vpOfToys = pres.AddRight(450);
-            vpOfToys.AddLeft(220);
-            vpOfToys.AddRight(260);
+            vpOfToys.AddLeft(250);
+            vpOfToys.AddRight(280);
+
+            var vpOfLuck = pres.AddLeft(400);
+            vpOfLuck.AddLeft(220);
+            vpOfLuck.AddRight(260);
 
             var vpOfMeetings = coo.AddLeft(380);
             vpOfMeetings.AddLeft(210);
